1a12eede0f7dff6b69a70680cd49989d6235b705
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2015-07-21 15:24:15 +0200

    Added add exercise action item

--- app/src/main/java/alobar/workout/views/NavigationFragment.java
+++ app/src/main/java/alobar/workout/views/NavigationFragment.java
@@ -4,8 +4,12 @@ package alobar.workout.views;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import alobar.workout.R;
 
@@ -14,18 +18,36 @@ import alobar.workout.R;
  */
 public class NavigationFragment extends Fragment {
 
-
     public NavigationFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_navigation, container, false);
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_exercises, menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addExerciseItem:
+                Toast.makeText(getActivity(), "Clicked", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
---
+++ assets/readme.md
@@ -0,0 +1,4 @@
+## Assets sources
+
+Some icons are from the Material design icons set:
+[https://www.google.com/design/icons/](https://www.google.com/design/icons/)
