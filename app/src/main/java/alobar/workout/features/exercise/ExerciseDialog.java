a412bd2e1882adc5ba6d9b1670291a50530482b7
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2016-10-02 17:46:32 +0200

    Added ExercisePresenter

--- app/src/main/java/alobar/workout/features/exercise/ExerciseDialog.java
+++ app/src/main/java/alobar/workout/features/exercise/ExerciseDialog.java
@@ -2,32 +2,47 @@ package alobar.workout.features.exercise;
 
 
 import android.app.Dialog;
-import android.app.DialogFragment;
+import android.app.Fragment;
 import android.content.ContentValues;
 import android.os.Bundle;
-import android.app.Fragment;
-import android.text.TextUtils;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.DialogFragment;
+import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import alobar.android.text.DebouncingTextWatcher;
 import alobar.workout.R;
 import alobar.workout.database.DatabaseContract;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ExerciseDialog extends DialogFragment implements View.OnClickListener {
+public class ExerciseDialog extends DialogFragment implements ExercisePresenter.View, View.OnClickListener {
 
+    private TextInputLayout nameInput;
     private EditText nameEdit;
+    private TextInputLayout weightInput;
     private EditText weightEdit;
+
+    private ExercisePresenter presenter;
 
     public ExerciseDialog() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        presenter = new ExercisePresenter(this);
+    }
+
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         Dialog result = super.onCreateDialog(savedInstanceState);
@@ -37,12 +52,29 @@ public class ExerciseDialog extends DialogFragment implements View.OnClickListen
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View result = inflater.inflate(R.layout.fragment_exercise_dialog, container, false);
+
+        nameInput = (TextInputLayout) result.findViewById(R.id.nameInput);
         nameEdit = (EditText) result.findViewById(R.id.nameEdit);
+        weightInput = (TextInputLayout) result.findViewById(R.id.weightInput);
         weightEdit = (EditText) result.findViewById(R.id.weightEdit);
+
+        nameEdit.addTextChangedListener(new DebouncingTextWatcher() {
+            @Override
+            public void onDebouncedTextChanged(Editable s) {
+                presenter.onNameChanged(s.toString());
+            }
+        });
+        weightEdit.addTextChangedListener(new DebouncingTextWatcher() {
+            @Override
+            public void onDebouncedTextChanged(Editable s) {
+                presenter.onWeightChanged(s.toString());
+            }
+        });
+
         result.findViewById(R.id.saveButton).setOnClickListener(this);
         result.findViewById(R.id.cancelButton).setOnClickListener(this);
+
         return result;
     }
 
@@ -50,10 +82,9 @@ public class ExerciseDialog extends DialogFragment implements View.OnClickListen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.saveButton:
-                if (validate()) {
-                    save();
-                    getDialog().dismiss();
-                }
+                String name = nameEdit.getText().toString().trim();
+                String weight = weightEdit.getText().toString().trim();
+                presenter.onSave(name, weight);
                 break;
             case R.id.cancelButton:
                 getDialog().cancel();
@@ -63,32 +94,42 @@ public class ExerciseDialog extends DialogFragment implements View.OnClickListen
         }
     }
 
-    private void save() {
-        String name = nameEdit.getText().toString().trim();
-        double weight = Double.parseDouble(weightEdit.getText().toString().trim());
-
+    public void saveToDatabase(String name, double weight) {
         ContentValues values = new ContentValues();
         values.put(DatabaseContract.Exercise.NAME, name);
         values.put(DatabaseContract.Exercise.WEIGHT, weight);
         getActivity().getContentResolver().insert(DatabaseContract.Exercise.CONTENT_URI, values);
     }
 
-    private boolean validate() {
-        StringBuilder messages = new StringBuilder();
+    @Override
+    public void setName(String value) {
+        nameEdit.setText(value);
+    }
 
-        String nameText = nameEdit.getText().toString().trim();
-        if (TextUtils.isEmpty(nameText)) {
-            messages.append("Name is required\n");
-        }
-        String weightText = weightEdit.getText().toString().trim();
-        if (TextUtils.isEmpty(weightText)) {
-            messages.append("Weight is required\n");
-        }
+    @Override
+    public void setNameHint(String message) {
+        nameInput.setError(message);
+        nameInput.setErrorEnabled(message != null);
+    }
 
-        boolean result = messages.length() == 0;
-        if (!result)
-            Toast.makeText(getActivity(), messages.toString(), Toast.LENGTH_LONG).show();
+    @Override
+    public void setWeight(String value) {
+        weightEdit.setText(value);
+    }
 
-        return result;
+    @Override
+    public void setWeightHint(String message) {
+        weightInput.setError(message);
+        weightInput.setErrorEnabled(message != null);
+    }
+
+    @Override
+    public void toastError(String message) {
+        Toast.makeText(getActivity(), message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void close() {
+        getDialog().dismiss();
     }
 }
--- app/src/main/java/alobar/workout/features/main/MainActivity.java
+++ app/src/main/java/alobar/workout/features/main/MainActivity.java
@@ -3,8 +3,12 @@ package alobar.workout.features.main;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import alobar.workout.R;
+import alobar.workout.features.exercise.ExerciseDialog;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +19,25 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.showOverflowMenu();
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.fragment_exercises, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+        switch (itemId) {
+            case R.id.addExerciseItem:
+                new ExerciseDialog().show(getSupportFragmentManager(), ExerciseDialog.class.getName());
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
