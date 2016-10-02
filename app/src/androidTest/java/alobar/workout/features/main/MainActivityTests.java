e53875dbd422786ade774e767ffbfc1c99b842db
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2016-10-02 19:29:36 +0200

    Added MainActivity Espresso test

---
+++ app/src/androidTest/java/alobar/workout/features/main/MainActivityTests.java
@@ -0,0 +1,29 @@
+package alobar.workout.features.main;
+
+import android.support.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import alobar.workout.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Tests for {@link MainActivity}
+ */
+public class MainActivityTests {
+
+    @Rule
+    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void shouldStart() {
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+    }
+}
