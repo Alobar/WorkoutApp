2d29aae41a273477f8294fbc8078c4d40dfcea26
Rob Meeuwisse <rob.meeuwisse@xs4all.nl>
2016-10-06 12:46:44 +0200

    Added content change notification uris to database contract

--- app/src/main/java/alobar/workout/db/DatabaseContract.java
+++ app/src/main/java/alobar/workout/db/DatabaseContract.java
@@ -1,22 +1,46 @@
 package alobar.workout.db;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
+
+import alobar.workout.BuildConfig;
 
 /**
  * Database contract
  */
 public class DatabaseContract {
 
+    private static final String Authority = BuildConfig.APPLICATION_ID + ".provider";
+
     /**
      * Constants for Exercise table.
      */
-    public interface Exercise {
+    public static class Exercise {
         /* Name of table */
-        String tableName = "exercise";
+        public static final String tableName = "exercise";
 
         // Content column names
-        String _ID = BaseColumns._ID;
-        String NAME = "name";
-        String WEIGHT = "weight";
+        public static final String _ID = BaseColumns._ID;
+        public static final String NAME = "name";
+        public static final String WEIGHT = "weight";
+
+        /**
+         * Content notification uri for exercises
+         *
+         * @return notification uri
+         */
+        public static Uri uri() {
+            return new Uri.Builder().scheme("content").authority(Authority).path(tableName).build();
+        }
+
+        /**
+         * Content notification uri for the specified exercise
+         *
+         * @param exerciseId the id of the exercise
+         * @return notification uri
+         */
+        public static Uri uri(long exerciseId) {
+            return Uri.withAppendedPath(uri(), Long.toString(exerciseId));
+        }
     }
 }
